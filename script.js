df3e13fbc74bdab64be8276d18596ed1bb2a2fe4
ryuryu1090122 <ryuryu1090122@gmail.com>
2024-12-19 13:47:23 +0900

    desktop

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 
+fetch('data/defenses.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(jsonData => {})
